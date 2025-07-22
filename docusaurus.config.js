e640881f094b572a46730e568423aac6ff72a89b
davidfrigolet <dfrigolet@gmail.com>
2025-07-22 19:14:41 +0100

    fix missing blog link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,10 +155,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
+              /*{
                 label: 'Blog',
                 to: '/blog',
-              },
+              },*/
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
